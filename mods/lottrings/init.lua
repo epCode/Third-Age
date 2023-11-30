06f67a3d26e0d980a47b9b0d220295837e760cda
epCode <eeliperry@gmail.com>
2023-11-29 21:41:37 -0700

    add testing ring

--- mods/lottrings/init.lua
+++ mods/lottrings/init.lua
@@ -2,12 +2,30 @@ lottrings = {
   ring_funcs = {}
 }
 
+--[[ Nazgul rings
+the names if all nine nazgul are:
+  Murazor -- The Witch King of Angmar
+  Khamûl
+  Dwar
+  Ji Indur
+  Akhorahil
+  Hoarmurath
+  Adunaphel
+  Ren
+  Uvatha
 
+Khamûl being the only named by Tolkien himself,
+the others are, however generally accepted,
+finding their origin in an old Middle-Earth
+series of role-play and card-trading games
+produced by Iron Crown Enterprises
+]]
 
 
 function lottrings.register_ring(name, def)
   minetest.register_craftitem("lottrings:"..name, { -- Testing Purpouses Only
     inventory_image = "lottrings_"..name..".png",
+    description = def.description or name:gsub("_", " "),
     groups = {armour_ring = 1},
     on_wear = def.on_wear,
     on_put_on = def.on_put_on or function() end,
@@ -19,14 +37,17 @@ function lottrings.register_ring(name, def)
   lottrings.ring_funcs["lottrings:"..name].on_take_off = def.on_take_off
 end
 
-lottrings.register_ring("basic_power_ring", { -- Testing Purpouses Only
+lottrings.register_ring("the_ring_of_murazor", { -- Testing Purpouses Only
+  description = "The Ring of Power of Murazor",
   on_wear = function(player)
   end,
   on_put_on = function(player)
-    print("Put on!")
+    minetest.chat_send_player(player:get_player_name(), minetest.colorize("#ff1111",
+    "*** You feel sure that you shall not be able to remove The Ring of Power of Murazor and live. ***"
+    ))
   end,
   on_take_off = function(player)
-    print("Taken off!")
+    player:punch(player, 1, {damage_groups={fleshy=1000}})
   end,
 })
 
