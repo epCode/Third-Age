656dac185d4ab978679808c7209c50f335391a1e
epCode <eeliperry@gmail.com>
2024-03-13 15:51:39 -0600

    lotttools first api commit

---
+++ mods/lotttools/crafting.lua
@@ -0,0 +1,50 @@
+local pattern_types = {
+  axe = {
+    blade = {
+      {"m", "m", "m"},
+      {"m", "m", ""},
+    },
+  },
+
+ pickaxe = {
+    blade = {
+      {"m", "m", "m"},
+      {"m", "", "m"},
+    },
+  },
+
+  sword = {
+    blade = {
+      {"", "", "m"},
+      {"", "m", ""},
+      {"m", "", ""},
+    },
+  },
+
+}
+
+function lotttools.register_craft(name, def)
+  local type = def.type
+  if not pattern_types[type] then minetest.log("warning", "Trying to register invalid tool type: "..tostring(type)) end
+
+  local blade_pattern = pattern_types[type].blade
+  local material = def.material
+
+  local theblade = {{},{},{}}
+
+  for index,row in pairs(blade_pattern) do
+    for i_,mat in pairs(blade_pattern[index]) do
+      if mat ~= "" then
+        theblade[index][i_] = material
+      else
+        theblade[index][i_] = ""
+      end
+    end
+  end
+
+
+  minetest.register_craft({
+      output = name,
+      recipe = theblade,
+  })
+end
--- mods/lotttools/init.lua
+++ mods/lotttools/init.lua
@@ -1,3 +1,36 @@
+lotttools = {}
+
+dofile(minetest.get_modpath("lotttools").."/crafting.lua")
+
+function lotttools.register_tool(name, def)
+
+	local modname = minetest.get_current_modname()
+
+	local type = def.type
+
+	minetest.register_tool(modname..":"..name, {
+		description = def.description or "Unknown Tool",
+		inventory_image = modname.."_"..name..",png",
+		tool_capabilities = def.tool_capabilities
+	})
+
+	lotttools.register_craft(modname..":"..name, def)
+end
+
+lotttools.register_tool("iron_sword", {
+	description = "Iron Sword",
+	material = "lottitems:apple",
+	type = "sword",
+	tool_capabilities = {
+		full_punch_interval = 0.5,
+		groupcaps = {
+			plant = {maxlevel = 0, uses = 100,
+				times = {[1] = 0.5, [2] = 0.75, [3] = 1, [4] = 0.2}
+			},
+		},
+	},
+})
+
 -- Tmp dig all pick!
 minetest.register_tool("lotttools:omni_pick", {
 	description = "Digs all!",
